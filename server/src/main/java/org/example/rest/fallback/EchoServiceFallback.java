ad42fc5a453061e0decee96f25b084e9046504c0
StevenChen <949727157@qq.com>
2020-03-31 17:56:10 +0800

    update

--- server/src/main/java/org/example/rest/fallback/EchoServiceFallback.java
+++ server/src/main/java/org/example/rest/fallback/EchoServiceFallback.java
@@ -6,12 +6,6 @@ import org.example.rest.EchoService;
 @Slf4j
 public class EchoServiceFallback implements EchoService {
 
-    private final Throwable throwable;
-
-    public EchoServiceFallback(Throwable throwable) {
-        this.throwable = throwable;
-    }
-
     @Override
     public String echo(String str) {
         return "网络好像有点问题！";
